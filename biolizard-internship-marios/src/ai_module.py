8faf249f348dce9a03f13f1a4c3797b64da268ad
MariosEft97 <marios.eftychiou97@gmail.com>
2022-09-05 10:41:23 +0300

    updated data_info and correlations functions

--- biolizard-internship-marios/src/ai_module.py
+++ biolizard-internship-marios/src/ai_module.py
@@ -1,83 +1,139 @@
-def data_import(folder, filename):
+def data_load(folder, filename):
     
-    '''The function imports the data and returns an appropriate data structure (Pandas DataFrame)'''
+    '''
+    The function loads the data and returns an appropriate data structure (Pandas DataFrame).
+
+    Parameters:
+        folder: path of the folder where the data are found
+        filename: name of the data file 
+    
+    Returns:
+        Pandas DataFrame with loaded data
+    '''
     
     import os
     import pandas as pd
   
     filepath = os.path.join(folder, filename)
     
-    data = ""
+    df = ""
     
     if filepath.endswith('.xlsx'):
-        data = pd.read_excel(filepath)
+        df = pd.read_excel(filepath)
     elif filepath.endswith('.csv'):
-        data = pd.read_csv(filepath)
+        df = pd.read_csv(filepath)
     elif filepath.endswith('.txt'):
-        data = pd.read_table(filepath)
+        df = pd.read_table(filepath)
     elif filepath.endswith('.json'):
-        data = pd.read_json(filepath)
+        df = pd.read_json(filepath)
     else:
         print(f"Error in {filename}")
         print("File extension is not correct.")
         print("Please specify a correct one (xlsx, csv, txt, json).")
     
-    return data
+    df.drop(df.filter(regex="Unnamed"), axis=1, inplace=True)
+
+    return df
     
-def data_info(df, name, threshold):
+def data_info(df, filename, threshold):
     
-    """The function displays important information about the DataFrames"""
+    """
+    The function creates and displays a report containing the following info:
+        1) Name of the data file
+        2) DataFrame dimensions (row, columns)
+        3) Categorical features (if present) and their unique values
+        4) Continuous features (if present) and their descriptive statistics
+        5) Missing data (if present)
     
-    import pandas as pd
+    Parameters:
+        df: Pandas DataFrame
+        filename: name of the data file
+        threshold (int): a feature having more unique values than the threshold is considered as continuous
+    
+    Returns:
+        categorical (list): list of categorical features in the DataFrame
+        continuous (list): list of continuous features in the DataFrame    
+    """
+
+    import numpy as np
     from tabulate import tabulate
 
-    # Number of rows and columns
+    # Number of entries and features
     
-    row_num = len(df)
-    column_num = len(df.columns)
+    entry_num = len(df)
+    feature_num = len(df.columns)
     
     print("\n")
-    print(f"{name}")
-    print("\n")
+    print(f"DATA FILE:")
     print(100*"-")
-    print(f"Rows: {row_num}")
-    print(f"Columns: {column_num}")
+    print(f"{filename}")
     print(100*"-")
     print("\n")
+    print("DIMENSIONS:")
+    print(100*"-")
+    print(f"Entries: {entry_num}")
+    print(f"Features: {feature_num}")
+    print(100*"-")
+    print("\n")
+    
+    # print("FEATURES:")
+    # print(100*"-")
+    # print(df.columns)
+    # print(100*"-")
+    # print("\n")
     
     # Categorical and Continuous features
     
     categorical = []
     continuous = []
+    identifier = []
 
-    categories = []
+    unique_values = []
+    continuous_statistics = []
 
     columns = list(df.columns)
 
     for column in columns:
-        if (len(df.loc[:,column].unique()) >= threshold):
+        if (len(df.loc[:,column].unique()) == entry_num) and (df[column].dtype == "object"):
+            identifier.append(column)
+        elif len(df.loc[:,column].unique()) >= threshold:
             continuous.append(column)
         else:
             categorical.append(column)
     
     for column in categorical:
-        categories.append([column, df[column].unique()])
+        unique_values.append([column, df[column].unique()])
+    
+    for column in continuous:
+        continuous_statistics.append(
+            [
+                column,
+                round(len(df[column]), 3),
+                round(np.mean(df[column]), 3),
+                round(np.std(df[column]), 3),
+                round(np.min(df[column]), 3),
+                round(np.percentile(df[column], q=25), 3),
+                round(np.median(df[column]), 3),
+                round(np.percentile(df[column], q=75), 3),
+                round(np.max(df[column]), 3)
+                ]
+                )
     
     print(f"CATEGORICAL FEATURES:")
     print(100*"-")
     
     if len(categorical)>=1:
-        # print("Feature: \t\t\t Categories:")
-        # for i, column in enumerate(categorical):
-        #     print(f"{i+1}: {column} \t\t\t {df[column].unique()}")
-        print(tabulate(categories, headers=["Features", "Categories"]))
+        print(tabulate(unique_values, headers=["Features", "Categories"]))
     else:
         print("There are no categorical features in the dataset.")
+    
     print(100*"-")
     print("\n")
+    
     print(f"CONTINUOUS FEATURES:")
     print(100*"-")
-    print(round(df[continuous].describe(), 3))
+
+    print(tabulate(continuous_statistics, headers=["Features", "Count", "Mean", "Std", "Min", "25th", "Median", "75th", "Max"]))
     print(100*"-")
     print("\n")
 
@@ -94,29 +150,63 @@ def data_info(df, name, threshold):
     for key, val in missing_data_dict.items():
         if val>0:
             with_na.append([key, val])
-            # print(f"Feature {key} has {val} missing values.")
         elif val==0:
             without_na.append([key, val])
-            # print(f"No missing values in feature {key}.")
     
     if len(with_na)>=1:
         print(f"The following features have missing values:")
         print(tabulate(with_na, headers=["Features", "Missing Data"]))
-        # for key, val in with_na.items():
-        #     print(f"{key}: \t {val} missing values.")
+        print("\n")
     else:
         pass
-    
-    print("\n")
-    
-    if len(without_na)==column_num:
+        
+    if len(without_na)==feature_num:
         print("There are no missing values in the dataset.")
     elif len(without_na)>=1:
         print(f"The following features do not have missing values:")
         print(tabulate(without_na, headers=["Features", "Missing Data"]))
-        # for key, val in without_na.items():
-        #     print(f"{key}")
     else:
         pass
     print(100*"-")
+
+    return categorical, continuous
+
+
+def correlations(df, type, printout):
+    
+    """
+    The function creates a correlation matrix/heatmap of the continuous features in the dataset.
+    
+    Parameters:
+        df: Pandas DataFrame
+        type (str): type of correlations (pearson or spearman)
+        printout (str): how correlations are displayed (matrix or heatmap)
+    
+    Returns:
+        None
+
+    """
+
+    import pandas as pd
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    import plotly.express as px
+    import plotly.io as pio
+    pio.renderers.default = "vscode"
+    from IPython.display import display
+
+    matrix = df.corr(method=type)
+
+    if printout == "matrix":
+        print("Correlation Matrix:")
+        display(round(matrix, 3))
+    elif printout == "heatmap":
+        print("Heatmap:")
+        fig = px.imshow(round(matrix, 3), text_auto=True, color_continuous_scale="Viridis")
+        fig.show()
+
+
+# def data_processing(df):
+
+
 
